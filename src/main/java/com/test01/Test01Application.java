25adb4070362880b7b9f57abe0129b26a9d1552c
蜗牛不会飞 <963094410@qq.com>
2023-04-11 13:08:49 +0800

    修改代码

--- src/main/java/com/test01/Test01Application.java
+++ src/main/java/com/test01/Test01Application.java
@@ -10,6 +10,9 @@ public class Test01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Test01Application.class, args);
+
+        int a=0;
     }
+    //test
 
 }
